6fac76738aebdc4f08f984b27b592cd1215ba174
wellyremigio <welly.remigio5@gmail.com>
2026-06-11 18:41:44 -0300

    chore: add inicial app structure

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Sorriso Libras API",
+    description="API do aplicativo Sorriso Libras",
+    version= "1.0.0",
+)
+
+@app.get("/")
+def read_root():
+    return {
+        "message": "API Sorriso libras funcionando!"
+    }
